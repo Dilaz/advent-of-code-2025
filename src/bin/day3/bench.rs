189bae114414f1988933eb5226ee0fe4527f65bd
Risto "Dilaz" Viitanen <risto.viitanen@iki.fi>
2025-12-03 23:15:03 +0200

    Fix day3 benchmarks

--- src/bin/day3/bench.rs
+++ src/bin/day3/bench.rs
@@ -8,15 +8,15 @@ fn main() {
 }
 
 #[divan::bench]
-fn bench_part1() -> Result<u32> {
+fn bench_part1() -> Result<u64> {
    Day3::part1(divan::black_box(include_str!(
-        "../../../inputs/day3.txt",
+        "../../../inputs/day3.txt"
     )))
 }
 
 #[divan::bench]
-fn bench_part2() -> Result<u32> {
+fn bench_part2() -> Result<u64> {
     Day3::part2(divan::black_box(include_str!(
-        "../../../inputs/day3.txt",
+        "../../../inputs/day3.txt"
     )))
 }
